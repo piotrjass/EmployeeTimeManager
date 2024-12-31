bc4cb76c1b28598ee63baae1e4bb92170123de2e
Piotr <piotrjasiulewiczkontakt@gmail.com>
2024-12-31 17:16:36 +0100

    Added .gitignore and cleaned up unnecessary files

--- HalfbitZadanie/Application/Queries/Employee/GetAllEmployeesQuery.cs
+++
@@ -1,8 +0,0 @@
-using MediatR;
-
-namespace HalfbitZadanie.Queries.Employee;
-
-public class GetAllEmployeesQuery : IRequest<List<Models.Employee>>
-{
-    
-}
--- HalfbitZadanie/Presentation/Models/Employee.cs
+++
@@ -1,9 +0,0 @@
-namespace HalfbitZadanie.Models;
-
-public class Employee
-{
-    public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-}
